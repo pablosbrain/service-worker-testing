f81737252374997c555993f92874acdceec7c5ca
Paul Lunneberg <paul@pablosbrain.com>
2018-06-12 23:34:02 -0500

    Added cache listing capabilities

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-// Check that service workers are registered
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', function(){
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
-
 document.addEventListener('DOMContentLoaded', function(e){ domReady(e); });
 
 var domReady = function(e){
   console.log("domReady", e);
-  document.querySelector("input[name=fetchButton]").addEventListener("click", fetchButtonEvent);
+  installServiceWorker(e);
+  document.querySelector("input[name=fetchButton]").addEventListener("click", fetchButtonEvent, false);
+  if('caches' in window) {
+    document.querySelector("body").classList.add("caches");
+    console.log("caches supported!");
+    viewCaches(e);
+    setInterval(function(){ viewCaches(); }, 3000);
+  }
+}
+
+var installServiceWorker = function(e){
+  if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
+      if(reg.installing) {
+        console.log('Service worker installing');
+      } else if(reg.waiting) {
+        console.log('Service worker installed');
+      } else if(reg.active) {
+        console.log('Service worker active');
+      }
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  }
+
 }
 
 var fetchButtonEvent = function(e){
-  console.log("fetchButtonEvent", e);
+  console.log("fetchButtonEvent");
+
   console.log("fetching", e.target.getAttribute("data-uri"));
   fetch(e.target.getAttribute("data-uri"))
   .then(function(response) { return response.json(); })
@@ -22,4 +41,36 @@ var fetchButtonEvent = function(e){
     console.log("json loaded")
     console.log(json);
   });
+
+}
+
+var viewCaches = function(e){
+  console.log("viewCaches");
+  let counter = document.querySelector("#cachecount");
+  counter.innerHTML = '0'
+  var cachedListEl = document.querySelector('#cachedList');
+  cachedListEl.innerHTML = ''; // clear it
+
+  if(document.querySelector("body").classList.contains("caches")){
+    caches.keys().then(function(cacheKeys) {
+      console.log("cacheKeys", cacheKeys);
+      cacheKeys.forEach(function(cacheKey, cacheindex){
+        caches.open(cacheKey).then(function(cache) {
+          // Cache is created / accessible
+          console.log("cache", cache);
+          cache.keys().then(function(cachedRequests) {
+            console.log(cachedRequests); // [Request, Request]
+            cachedRequests.forEach(function(cachedReq, cachedReqIndex){
+              let newLi = document.createElement("li");
+              let newLiVal = document.createTextNode(cachedReq.url);
+              newLi.appendChild(newLiVal);
+              cachedListEl.appendChild(newLi);
+              counter.innerHTML =parseInt(counter.innerHTML)+1;
+            });
+          });
+        });
+      });
+    });
+
+  }
 }
--- sw.js
+++ sw.js
@@ -7,29 +7,10 @@ var cachePrefix = "testingV"+cacheVersion;
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
   workbox.setConfig({debug: true});
-  workbox.routing.registerRoute(
-    "/",
-    workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' })
-  );
-  workbox.routing.registerRoute(
-    "/?src=pwa",
-    workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' })
-  );
-  workbox.routing.registerRoute(
-    /.*\.js/,
-    workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' })
-  );
-  workbox.routing.registerRoute(
-    /.*\.css/,
-    workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' })
-  );
-  workbox.routing.registerRoute(
-    /.*\.png/,
-    workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' })
-  );
-
-  workbox.routing.registerRoute(
-    /.*\.json/,
-    workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-content-cache' })
-  );
+  workbox.routing.registerRoute("/", workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' }));
+  workbox.routing.registerRoute("/?src=pwa", workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' }));
+  workbox.routing.registerRoute(/.*\.json/, workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-content-cache' }));
+  workbox.routing.registerRoute(/.*\.js/, workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' }));
+  workbox.routing.registerRoute(/.*\.css/, workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' }));
+  workbox.routing.registerRoute(/.*\.png/, workbox.strategies.staleWhileRevalidate({ cacheName: cachePrefix+'-cache' }));
 }
